e6c26d4036653c9b183e85cbbbd71ae94eb9ceeb
Lou <72024456+lunars5@users.noreply.github.com>
2022-05-03 00:14:50 +0100

    Add files via upload

---
+++ about.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Northampton Chocolates</title>
+  <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="container">
+  <nav>
+    <div logo><img src="/dist/images/pop.gif" alt="Logo"/></div>
+    <ul>
+      <li>Home</li>
+      <li>About Us</li>
+      <li>Products</li>
+      <li>Contact Us</li>
+    </ul>
+  </nav>
+  <main>
+    <div class="container"> </div>
+
+    <p> Northampton Speciality Chocloates is a family founded, luxury, handmade chocolate supplier and chocolatier. Based in Northampton, our chocolatiers work to provide a variety of sweet treats for your every need! We are a brand new buisness that makes and 
+      supplies a range of different chocolates, organically sourced with gluten free and vegan options. We would be happy to suit your personal requests, special gift ideas, personalisation and dietary requirements using our contact us page. Buy chocolate gifts, 
+      chocolate hampers, gift boxes aSnd personalised gifts for someone special.. or induldge yourself. Chocolate delivery direct to your door.
+    </p>
+    <h2>other section</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi aspernatur maiores rem velit totam praesentium iure, neque autem explicabo fuga odio. Tempora harum reiciendis totam tenetur reprehenderit tempore debitis laudantium!
+    Et quibusdam natus aperiam, nam sed illum quae vel libero rerum aut provident architecto repellendus ipsum possimus. Iure, sequi maxime. Beatae id libero unde culpa necessitatibus eaque blanditiis laudantium voluptate.
+    Enim laudantium autem atque sint dicta ullam, quod, dolorum architecto dolores harum quaerat vero, obcaecati vitae consequuntur laboriosam nostrum non pariatur asperiores reprehenderit voluptatum veritatis aliquam iusto expedita! Blanditiis, laboriosam!
+    Recusandae, reiciendis eveniet, amet rerum perspiciatis est tenetur, numquam officia consequatur aperiam dolor quasi sunt repudiandae repellat! Perferendis magni, asperiores esse expedita, deserunt maiores illo, fugiat voluptates ipsum rerum quis?
+    Reiciendis excepturi, iste cupiditate necessitatibus et quos ipsa nesciunt quia doloribus quas molestiae, quisquam voluptas cum totam sequi error labore, ducimus eligendi eius nostrum in non laborum! Est, consequatur tempora!
+    Amet voluptatem, repudiandae, numquam maxime beatae inventore voluptas aspernatur, recusandae natus perspiciatis aperiam. Exercitationem repellendus delectus tempora quo rerum, iure labore debitis, dolores aut inventore, hic blanditiis aliquid fugit sint.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi aspernatur maiores rem velit totam praesentium iure, neque autem explicabo fuga odio. Tempora harum reiciendis totam tenetur reprehenderit tempore debitis laudantium!
+    Et quibusdam natus aperiam, nam sed illum quae vel libero rerum aut provident architecto repellendus ipsum possimus. Iure, sequi maxime. Beatae id libero unde culpa necessitatibus eaque blanditiis laudantium voluptate.
+    Enim laudantium autem atque sint dicta ullam, quod, dolorum architecto dolores harum quaerat vero, obcaecati vitae consequuntur laboriosam nostrum non pariatur asperiores reprehenderit voluptatum veritatis aliquam iusto expedita! Blanditiis, laboriosam!
+    Recusandae, reiciendis eveniet, amet rerum perspiciatis est tenetur, numquam officia consequatur aperiam dolor quasi sunt repudiandae repellat! Perferendis magni, asperiores esse expedita, deserunt maiores illo, fugiat voluptates ipsum rerum quis?
+    Reiciendis excepturi, iste cupiditate necessitatibus et quos ipsa nesciunt quia doloribus quas molestiae, quisquam voluptas cum totam sequi error labore, ducimus eligendi eius nostrum in non laborum! Est, consequatur tempora!
+    Amet voluptatem, repudiandae, numquam maxime beatae inventore voluptas aspernatur, recusandae natus perspiciatis aperiam. Exercitationem repellendus delectus tempora quo rerum, iure labore debitis, dolores aut inventore, hic blanditiis aliquid fugit sint.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi aspernatur maiores rem velit totam praesentium iure, neque autem explicabo fuga odio. Tempora harum reiciendis totam tenetur reprehenderit tempore debitis laudantium!
+    Et quibusdam natus aperiam, nam sed illum quae vel libero rerum aut provident architecto repellendus ipsum possimus. Iure, sequi maxime. Beatae id libero unde culpa necessitatibus eaque blanditiis laudantium voluptate.
+    Enim laudantium autem atque sint dicta ullam, quod, dolorum architecto dolores harum quaerat vero, obcaecati vitae consequuntur laboriosam nostrum non pariatur asperiores reprehenderit voluptatum veritatis aliquam iusto expedita! Blanditiis, laboriosam!
+    Recusandae, reiciendis eveniet, amet rerum perspiciatis est tenetur, numquam officia consequatur aperiam dolor quasi sunt repudiandae repellat! Perferendis magni, asperiores esse expedita, deserunt maiores illo, fugiat voluptates ipsum rerum quis?
+    Reiciendis excepturi, iste cupiditate necessitatibus et quos ipsa nesciunt quia doloribus quas molestiae, quisquam voluptas cum totam sequi error labore, ducimus eligendi eius nostrum in non laborum! Est, consequatur tempora!
+    Amet voluptatem, repudiandae, numquam maxime beatae inventore voluptas aspernatur, recusandae natus perspiciatis aperiam. Exercitationem repellendus delectus tempora quo rerum, iure labore debitis, dolores aut inventore, hic blanditiis aliquid fugit sint.</p>
+      
+
+
+    
+  </main>
+  <footer>©2022 Northampton Speciality Chocolate. 
+    All Rights Reserved. Privacy and Terms of Service</footer>
+</div>
+<!-- partial -->
+  
+</body>
+</html>
