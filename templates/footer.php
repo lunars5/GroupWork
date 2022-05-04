881dab6140f2e966323e6456e0099bf034708237
MkMaxxi <45230525+MkMaxxi@users.noreply.github.com>
2022-05-04 10:11:36 +0100

    template folder
    templates for the navbar and footer, to make things a little bit easier going forward.

---
+++ templates/footer.php
@@ -0,0 +1,7 @@
+<footer>©2022 Northampton Speciality Chocolate. 
+    All Rights Reserved. Privacy and Terms of Service</footer>
+</div>
+<!-- partial -->
+  
+</body>
+</html>
---
+++ templates/navbar.php
@@ -0,0 +1,14 @@
+<nav>
+    <a href="index.php"><img src="../images/Logo.png" alt="Logo" class="logo"/></a>
+    <ul>
+      
+      <li><a href="index.php">Home</a></li>
+      <li><a href="about.php">About</a></li>
+      <li><a href="products.php">Products</a></li>
+      <li><a href="contact.php">Contact Us</a></li>
+      <li><a href="login.php"><img src ="/images/usericon.png" alt="User" class="usericon"></a></li>
+      <li><a href="cart.php"><img src ="/images/shoppingcart.png" alt="Shopping Cart" class="shoppingcart"></a></li>
+      <input type="text" placeholder="Search">
+    </ul>
+    
+  </nav>
