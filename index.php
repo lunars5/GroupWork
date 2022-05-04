a7ebb50f4cd7b73a7e7d678ef86ce1e1e67c2bbd
kmario158 <74537823+kmario158@users.noreply.github.com>
2022-05-04 10:04:22 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -10,14 +10,20 @@
 <!-- partial:index.partial.html -->
 <div class="container">
   <nav>
-    <div logo><img src="/dist/images/pop.gif" alt="Logo"/></div>
+    <a href="index.php"><img src="/images/Logo.png" alt="Logo" class="logo"/></a>
     <ul>
-      <li>Home</li>
-      <li>About Us</li>
-      <li>Products</li>
-      <li>Contact Us</li>
+      
+      <li><a href="index.php">Home</a></li>
+      <li><a href="about.php">About</a></li>
+      <li><a href="products.php">Products</a></li>
+      <li><a href="contact.php">Contact Us</a></li>
+      <li><a href="login.php"><img src ="/images/usericon.png" alt="User" class="usericon"></a></li>
+      <li><a href="cart.php"><img src ="/images/shoppingcart.png" alt="Shopping Cart" class="shoppingcart"></a></li>
+      <input type="text" placeholder="Search">
     </ul>
+    
   </nav>
+
   <main>
     <div class="container"> </div>
 
