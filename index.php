93d4fce9525166e4dcc147e17fc5baf60f4fef65
MkMaxxi <45230525+MkMaxxi@users.noreply.github.com>
2022-05-04 10:17:20 +0100

    template edit
    cleaned up the code on the index page by using php require statements to link to the templates I made. we can use this version of the index page to assist in the creation of future pages

--- index.php
+++ index.php
@@ -1,32 +1,19 @@
+
 <!DOCTYPE html>
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
   <title>Northampton Chocolates</title>
   <link rel="stylesheet" href="./style.css">
-
 </head>
 <body>
 <!-- partial:index.partial.html -->
 <div class="container">
-  <nav>
-    <a href="index.php"><img src="/images/Logo.png" alt="Logo" class="logo"/></a>
-    <ul>
-      
-      <li><a href="index.php">Home</a></li>
-      <li><a href="about.php">About</a></li>
-      <li><a href="products.php">Products</a></li>
-      <li><a href="contact.php">Contact Us</a></li>
-      <li><a href="login.php"><img src ="/images/usericon.png" alt="User" class="usericon"></a></li>
-      <li><a href="cart.php"><img src ="/images/shoppingcart.png" alt="Shopping Cart" class="shoppingcart"></a></li>
-      <input type="text" placeholder="Search">
-    </ul>
-    
-  </nav>
-
+<?php
+  require 'templates/navbar.php';
+  ?>
   <main>
     <div class="container"> </div>
-
     <p> Northampton Speciality Chocloates is a family founded, luxury, handmade chocolate supplier and chocolatier. Based in Northampton, our chocolatiers work to provide a variety of sweet treats for your every need! We are a brand new buisness that makes and 
       supplies a range of different chocolates, organically sourced with gluten free and vegan options. We would be happy to suit your personal requests, special gift ideas, personalisation and dietary requirements using our contact us page. Buy chocolate gifts, 
       chocolate hampers, gift boxes and personalised gifts for someone special.. or induldge yourself. Chocolate delivery direct to your door.
@@ -51,14 +38,8 @@
     Reiciendis excepturi, iste cupiditate necessitatibus et quos ipsa nesciunt quia doloribus quas molestiae, quisquam voluptas cum totam sequi error labore, ducimus eligendi eius nostrum in non laborum! Est, consequatur tempora!
     Amet voluptatem, repudiandae, numquam maxime beatae inventore voluptas aspernatur, recusandae natus perspiciatis aperiam. Exercitationem repellendus delectus tempora quo rerum, iure labore debitis, dolores aut inventore, hic blanditiis aliquid fugit sint.</p>
       
-
-
     
   </main>
-  <footer>©2022 Northampton Speciality Chocolate. 
-    All Rights Reserved. Privacy and Terms of Service</footer>
-</div>
-<!-- partial -->
-  
-</body>
-</html>
+<?php
+require 'templates/footer.php';
+?>
