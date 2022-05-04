f032f3d34eed8ca6cb53874e8a8edb6165ce5699
kmario158 <74537823+kmario158@users.noreply.github.com>
2022-05-04 11:42:03 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -1,67 +1,82 @@
 <?php
+ob_start();
 	session_start();
+	require 'database.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css"/>
-    <title>SignUp</title>
+    <link rel="stylesheet" href="style.css"/>
+    <title>Log in</title>
 </head>
-<body>
-<form action= "index.php" method= "POST">
-	<p>Log In:</p>
-	<label>User Name</label> <input type="text" placeholder = "test"  name="userName"/>
-	<label>Password</label> <input type="text" placeholder = "test" name="password"/>
-
-	<input type = "submit" name = "submit" value = "Submit" />
-</form>
+<body class="loginbody">
+<img src="logo.png" alt="Logo" class="loginlogo">
 
 <?php
-	$server = 'mysql';
-	$username = 'student';
-	$password = 'student';
-	$schema = 'mydb';
 
-	$pdo = new PDO('mysql:dbname=' . $schema . ';host=' . $server, $username, $password);
+	if(isset($_POST['submit'])){
 
-	
-
-	$user = $pdo -> prepare('SELECT * FROM users WHERE userName = :userName AND password = :password AND user_type = :user');
+	$user = $pdo -> prepare('SELECT * FROM users WHERE username = :username AND password = :password AND user_type = :user');
 
 	$userInfo = [
-		'userName' => $_POST['userName'],
+		'username' => $_POST['username'],
 		'password' => $_POST['password'],
 		'user' => 'user'
 	];
 	$user -> execute($userInfo);
 
-	$admin = $pdo -> prepare('SELECT * FROM users WHERE username = :userName AND password = :password AND user_type = :admin');
+	$admin = $pdo -> prepare('SELECT * FROM users WHERE username = :username AND password = :password AND user_type = :admin');
 
 	$adminInfo = [
-		'userName' => $_POST['username'],
+		'username' => $_POST['username'],
 		'password' => $_POST['password'],
 		'admin' => 'admin'
 	];
 	$admin -> execute($adminInfo);
 
 	if ($admin -> rowCount() > 0) {
-		$_SESSION['admin'] = true;
-		echo '<p>admin</p>';
+		$_SESSION['admin'] == true;
+		$_SESSION['loggedin'] == true;
+		echo '<p>Admin logged in</p>';
+		header("Location: index.php");
+    	exit();
 	   }
 
-	   if ($user -> rowCount() > 0){
-		   $_SESSION['user'] = true;
-		   echo '<p>user</p>';
+	   else if ($user -> rowCount() > 0){
+		   $_SESSION['user'] == true;
+		   $_SESSION['loggedin'] == true;
+		   echo '<p>User logged in</p>';
 	   }
 
 	   else {
-		echo '<p>username or password is incorrect</p>';
+		echo '<p class="errormessage">Username or password is incorrect</p>';
 	   }
+	}
+	else{
 ?>
 
-<footer> &copy;2022 Northampton Speciality Chocolate. 
-All Rights Reserved. Privacy and Terms of Service</footer>
+<div class='bold-line'></div>
+<div class='wrapper'>
+  <div class='window2'>
+    <div class='overlay'></div>
+    <div class='content'>
+      <div class='input-fields'>
+	  <form action= "login.php" method= "POST">
+        <input type='text' placeholder='Username' class='input-line full-width' name="username"/>
+        <input type='password' placeholder='Password' class='input-line full-width' name="password"/>
+
+      </div>
+      <div><input type="submit" class='ghost-round full-width' name="submit" value="Submit" /></div>
+    </div>
+  </div>
+</div>
+	</form>
+<?php
+	}
+?>
+<footer>
+			&copy; 2022 Northampton Speciality Chocolate. 
+All Rights Reserved. Privacy and Terms of Service
+		</footer>
 </body>
 </html>
